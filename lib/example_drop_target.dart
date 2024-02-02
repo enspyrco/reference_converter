b08a0c5991d929d676e29064ad2348e51c02ed91
Nick Meinhold <nick@enspyr.co>
2024-02-02 18:28:35 +1100

    Add sign out button

--- lib/example_drop_target.dart
+++ lib/example_drop_target.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 import 'package:cross_file/cross_file.dart';
 import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterfire_firebase_auth_for_perception/flutterfire_firebase_auth_for_perception.dart';
+
+import 'app/app_beliefs.dart';
 
 class ExampleDragTarget extends StatefulWidget {
   const ExampleDragTarget({super.key});
@@ -18,38 +21,43 @@ class _ExampleDragTargetState extends State<ExampleDragTarget> {
 
   @override
   Widget build(BuildContext context) {
-    return DropTarget(
-      onDragDone: (details) {
-        setState(() {
-          for (final file in details.files) {
-            debugPrint("uri: ${file.path} "
-                "${File(file.path).existsSync()}");
-          }
-          _files.addAll(details.files);
-        });
-      },
-      onDragEntered: (details) {
-        if (!mounted) return;
+    return Column(
+      children: [
+        const SignOutOfFirebaseButton<AppBeliefs>(),
+        DropTarget(
+          onDragDone: (details) {
+            setState(() {
+              for (final file in details.files) {
+                debugPrint("uri: ${file.path} "
+                    "${File(file.path).existsSync()}");
+              }
+              _files.addAll(details.files);
+            });
+          },
+          onDragEntered: (details) {
+            if (!mounted) return;
 
-        setState(() {
-          _dragging = true;
-        });
-      },
-      onDragExited: (details) {
-        if (!mounted) return;
+            setState(() {
+              _dragging = true;
+            });
+          },
+          onDragExited: (details) {
+            if (!mounted) return;
 
-        setState(() {
-          _dragging = false;
-        });
-      },
-      child: Container(
-        height: 200,
-        width: 200,
-        color: _dragging ? Colors.blue.withOpacity(0.4) : Colors.black26,
-        child: _files.isEmpty
-            ? const Center(child: Text("Drop here"))
-            : Text(_files.map((f) => f.name).join("\n")),
-      ),
+            setState(() {
+              _dragging = false;
+            });
+          },
+          child: Container(
+            height: 200,
+            width: 200,
+            color: _dragging ? Colors.blue.withOpacity(0.4) : Colors.black26,
+            child: _files.isEmpty
+                ? const Center(child: Text("Drop here"))
+                : Text(_files.map((f) => f.name).join("\n")),
+          ),
+        ),
+      ],
     );
   }
 }
